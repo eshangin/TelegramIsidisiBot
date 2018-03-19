5a6ca1f5733eedeb6799040cea238745ec6343c0
eshangin <shangin.jekon@mail.ru>
2018-03-19 22:19:52 +0700

    use windows service to run main script

---
+++ startService.js
@@ -0,0 +1,30 @@
+require('dotenv').config({path: './.env.txt'});
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name: 'IsidisiService',
+  description: 'The nodejs.org example web server.',
+  script: 'index.js',
+  env: [{
+    name: "BOT_TOKEN",
+    value: process.env.BOT_TOKEN
+  }]
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+// Install after uninstall. As a result the service will be reinstalled.
+svc.on('uninstall',function(){
+  svc.install();
+});
+
+if (svc.exists) {
+  svc.uninstall();
+} else {
+  svc.install();
+}
