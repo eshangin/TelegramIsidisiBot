c61e0800eff3872af836339a4415e1da12c5bb08
eshangin <shangin.jekon@mail.ru>
2018-03-21 02:25:16 +0700

    more fixes

--- reinstallService.js
+++ reinstallService.js
@@ -1,10 +1,10 @@
 const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '.env.txt')});
+require('dotenv').config({path: './.env.txt'});
 var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name: path.join(__dirname, process.env.WIN_SERVICE_NAME),
+  name: path.join(process.env.WIN_SERVISE_APP_PATH, process.env.WIN_SERVICE_NAME),
   description: process.env.WIN_SERVICE_DESCR,
   script: process.env.WIN_SERVICE_START_SCRIPT,
   env: [{
