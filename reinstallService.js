2cf1c78aca8b12a72dae7eb86201b699b963bfca
eshangin <shangin.jekon@mail.ru>
2018-03-21 02:00:36 +0700

    fix path to bot logic file in service

--- reinstallService.js
+++ reinstallService.js
@@ -1,9 +1,10 @@
+const path = require('path');
 require('dotenv').config({path: './.env.txt'});
 var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name: process.env.WIN_SERVICE_NAME,
+  name: path.join(__dirname, process.env.WIN_SERVICE_NAME),
   description: process.env.WIN_SERVICE_DESCR,
   script: process.env.WIN_SERVICE_START_SCRIPT,
   env: [{
