cc83ca213a4cea1564d73da416a1513cf47729e3
eshangin <shangin.jekon@mail.ru>
2018-03-21 02:41:28 +0700

    fix!?

--- reinstallService.js
+++ reinstallService.js
@@ -4,9 +4,9 @@ var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name: path.join(process.env.WIN_SERVISE_APP_PATH, process.env.WIN_SERVICE_NAME),
+  name: process.env.WIN_SERVICE_NAME,
   description: process.env.WIN_SERVICE_DESCR,
-  script: process.env.WIN_SERVICE_START_SCRIPT,
+  script: path.join(process.env.WIN_SERVISE_APP_PATH, process.env.WIN_SERVICE_START_SCRIPT),
   env: [{
     name: "BOT_TOKEN",
     value: process.env.BOT_TOKEN
--- uninstallService.js
+++ uninstallService.js
@@ -4,8 +4,8 @@ var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name: path.join(process.env.WIN_SERVISE_APP_PATH, process.env.WIN_SERVICE_NAME),
-  script: process.env.WIN_SERVICE_START_SCRIPT
+  name: process.env.WIN_SERVICE_NAME,
+  script: path.join(process.env.WIN_SERVISE_APP_PATH, process.env.WIN_SERVICE_START_SCRIPT)
 });
 
 svc.uninstall();
